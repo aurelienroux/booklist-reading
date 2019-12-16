d09671b34de3b778131a4ccda87bab51b5c22953
Aurélien Roux <aurelien.roux@aerial.ai>
2019-12-15 20:48:28 -0500

    create booklist and bookdetails comp

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import BookContextProvider from './Contexts/BookContext'
 import NavBar from './Components/NavBar'
+import BookList from './Components/BookList'
 
 function App() {
   return (
     <div className="App">
       <BookContextProvider>
         <NavBar />
+        <BookList />
       </BookContextProvider>
     </div>
   )
