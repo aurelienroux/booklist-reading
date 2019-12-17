aa945d7487b40aea38cd9fe5ddc23ec44dcf7403
Aurélien Roux <aurelien.roux@aerial.ai>
2019-12-16 21:01:45 -0500

    useReducer example

--- src/Contexts/BookContext.js
+++ src/Contexts/BookContext.js
@@ -1,24 +1,13 @@
-import React, { createContext, useState } from 'react'
-import uuid from 'uuid'
+import React, { createContext, useReducer } from 'react'
+import { bookReducer } from '../Reducers/bookReducer'
 
 export const BookContext = createContext()
 
 const BookContextProvider = props => {
-  const [books, setBooks] = useState([
-    { title: 'book 1', author: 'author 1', id: 1 },
-    { title: 'book 2', author: 'author 2', id: 2 },
-    { title: 'book 3', author: 'author 3', id: 3 },
-    { title: 'book 4', author: 'author 4', id: 4 }
-  ])
-  const addBook = (title, author) => {
-    setBooks([...books, { title, author, id: uuid() }])
-  }
-  const removeBook = id => {
-    setBooks(books.filter(book => book.id !== id))
-  }
+  const [books, dispatch] = useReducer(bookReducer, [])
 
   return (
-    <BookContext.Provider value={{ books, addBook, removeBook }}>
+    <BookContext.Provider value={{ books, dispatch }}>
       {props.children}
     </BookContext.Provider>
   )
